ccf55ed98d00d5528d7a72b5598f3327cbfc6d38
Ken Wills <ken@zaptillion.net>
2018-05-23 16:22:18 -0500

    add back embedded mq

--- jboss-eap-cd-openshift-launch/configure.sh
+++ jboss-eap-cd-openshift-launch/configure.sh
@@ -6,5 +6,3 @@ ADDED_DIR=${SCRIPT_DIR}/added
 
 # Add custom configuration file
 cp -p ${ADDED_DIR}/standalone-openshift.xml $JBOSS_HOME/standalone/configuration/
-# removes the default embedded broker
-cp -p ${ADDED_DIR}/launch/messaging.sh $JBOSS_HOME/bin/launch/
