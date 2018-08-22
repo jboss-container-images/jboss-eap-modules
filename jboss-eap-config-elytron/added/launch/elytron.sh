1581e0767137bd85acf54b6dd4f40d703abbf210
Ken Wills <ken@zaptillion.net>
2018-08-22 17:40:32 -0500

    update to support legacy config

--- jboss-eap-config-elytron/added/launch/elytron.sh
+++ jboss-eap-config-elytron/added/launch/elytron.sh
@@ -33,27 +33,69 @@ function configure_https() {
       key_password="<credential-reference clear-text=\"${HTTPS_KEY_PASSWORD}\"/>"
     fi
 
-    key_store="<key-store name=\"LocalhostKeyStore\">\n\
+    legacy_elytron_tls="<tls>\n\
+        <key-stores>\n\
+            <key-store name=\"LocalhostKeyStore\">\n\
                 <credential-reference clear-text=\"${HTTPS_PASSWORD}\"/>\n\
                 <implementation type=\"${HTTPS_KEYSTORE_TYPE}\"/>\n\
-                <file path=\"${HTTPS_KEYSTORE}\" relative-to=\"${HTTPS_KEYSTORE_DIR}\"/>\n\
+                <file path=\"${HTTPS_KEYSTORE_DIR}/${HTTPS_KEYSTORE}\"/>\n\
+            </key-store>\n\
+        </key-stores>\n\
+        <key-managers>\n\
+            <key-manager name=\"LocalhostKeyManager\" key-store=\"LocalhostKeyStore\">\n\
+                $key_password\n\
+            </key-manager>\n\
+        </key-managers>\n\
+        <server-ssl-contexts>\n\
+            <server-ssl-context name=\"LocalhostSslContext\" key-manager=\"LocalhostKeyManager\"/>\n\
+        </server-ssl-contexts>\n\
+    </tls>"
+
+    elytron_tls="<tls>\n\
+                <key-stores>\n\
+                    <!-- ##ELYTRON_KEY_STORE## -->\n\
+                </key-stores>\n\
+                <key-managers>\n\
+                    <!-- ##ELYTRON_KEY_MANAGER## -->\n\
+                </key-managers>\n\
+                <server-ssl-contexts>\n\
+                    <!-- ##ELYTRON_SERVER_SSL_CONTEXT## -->\n\
+                </server-ssl-contexts>\n\
+            </tls>\n"
+
+    elytron_key_store="<key-store name=\"LocalhostKeyStore\">\n\
+                <credential-reference clear-text=\"${HTTPS_PASSWORD}\"/>\n\
+                <implementation type=\"${HTTPS_KEYSTORE_TYPE}\"/>\n\
+                <file path=\"${HTTPS_KEYSTORE_DIR}/${HTTPS_KEYSTORE}\"/>\n\
             </key-store>\n"
 
-    key_manager="<key-manager name=\"LocalhostKeyManager\" key-store=\"LocalhostKeyStore\">$key_password</key-manager>\n"
-    server_ssl_contexts="<server-ssl-context name=\"LocalhostSslContext\" key-manager=\"LocalhostKeyManager\"/>\n"
-    https_connector="<https-listener name=\"https\" socket-binding=\"https\" ssl-context=\"LocalhostSslContext\" proxy-address-forwarding=\"true\"/>"
+    elytron_key_manager="<key-manager name=\"LocalhostKeyManager\" key-store=\"LocalhostKeyStore\">$key_password</key-manager>\n"
+    elytron_server_ssl_context="<server-ssl-context name=\"LocalhostSslContext\" key-manager=\"LocalhostKeyManager\"/>\n"
+    elytron_https_connector="<https-listener name=\"https\" socket-binding=\"https\" ssl-context=\"LocalhostSslContext\" proxy-address-forwarding=\"true\"/>"
 
   elif [ -n "${HTTPS_PASSWORD}" -o -n "${HTTPS_KEYSTORE_DIR}" -o -n "${HTTPS_KEYSTORE}" -o -n "${HTTPS_KEYSTORE_TYPE}" ]; then
     echo "WARNING! Partial HTTPS configuration, the https connector WILL NOT be configured."
   fi
 
-  sed -i "s|<!-- ##ELYTRON_KEY_STORE## -->|${key_store}<!-- ##ELYTRON_KEY_STORE## -->|" $CONFIG_FILE
-  sed -i "s|<!-- ##ELYTRON_KEY_MANAGER## -->|${key_manager}<!-- ##ELYTRON_KEY_MANAGER## -->|" $CONFIG_FILE
-  sed -i "s|<!-- ##ELYTRON_SERVER_SSL_CONTEXT## -->|${key_manager}<!-- ##ELYTRON_SERVER_SSL_CONTEXT## -->|" $CONFIG_FILE
-  sed -i "s|<!-- ##HTTPS_CONNECTOR## -->|${https_connector}<!-- ##HTTPS_CONNECTOR## -->|" $CONFIG_FILE
+  # check for new config tag, use that if it's present
+  grep "<!-- ##ELYTRON_TLS## -->" $CONFIG_FILE
+  has_elytron_tls=$?
+  if [ ${has_elytron_tls} -eq "0" ]; then # matches, its the new config
+    sed -i "s|<!-- ##ELYTRON_TLS## -->|${elytron_tls}|" $CONFIG_FILE
+    # remove the legacy tag, if it's present
+    sed -i "s|<!-- ##TLS## -->||" $CONFIG_FILE
+  fi
 
-  # old var
-  #sed -i "s|<!-- ##TLS## -->|${tls}|" $CONFIG_FILE
+  grep "<!-- ##ELYTRON_KEY_STORE## -->" $CONFIG_FILE
+  has_elytron_key_store=$?
+  if [ ${has_elytron_key_store} -eq "0" ]; then # not already added
+    sed -i "s|<!-- ##ELYTRON_KEY_STORE## -->|${elytron_key_store}<!-- ##ELYTRON_KEY_STORE## -->|" $CONFIG_FILE
+    sed -i "s|<!-- ##ELYTRON_KEY_MANAGER## -->|${elytron_key_manager}<!-- ##ELYTRON_KEY_MANAGER## -->|" $CONFIG_FILE
+    sed -i "s|<!-- ##ELYTRON_SERVER_SSL_CONTEXT## -->|${elytron_server_ssl_context}<!-- ##ELYTRON_SERVER_SSL_CONTEXT## -->|" $CONFIG_FILE
+  else # legacy config
+    sed -i "s|<!-- ##TLS## -->|${legacy_elytron_tls}|" $CONFIG_FILE
+  fi
+  sed -i "s|<!-- ##HTTPS_CONNECTOR## -->|${elytron_https_connector}<!-- ##HTTPS_CONNECTOR## -->|" $CONFIG_FILE
 }
 
 function configure_security_domains() {
