67d3b46f8300544c29195ba5f663cb2bdc2ceda1
Brian Stansberry <brian.stansberry@redhat.com>
2019-03-28 17:11:11 -0500

    Merge pull request #67 from luck3y/fix_bats_tests

--- os-eap7-launch/tests/bats/datasource/common.bash
+++ os-eap7-launch/tests/bats/datasource/common.bash
@@ -1,12 +1,13 @@
-load $BATS_TEST_DIRNAME/../../../../tests/bats/common/xml_utils.bash
-load $BATS_TEST_DIRNAME/../../../../tests/bats/common/log_utils.bash
+echo $BATS_TEST_DIRNAME
+load $BATS_TEST_DIRNAME/../../../../test-common/xml_utils.bash
+load $BATS_TEST_DIRNAME/../../../../test-common/log_utils.bash
 
 export JBOSS_HOME=$BATS_TMPDIR/jboss_home
 export CONFIG_FILE=$JBOSS_HOME/standalone/configuration/standalone-openshift.xml
 
 mkdir -p $JBOSS_HOME/bin/launch
 cp $BATS_TEST_DIRNAME/../../../../os-eap-node-name/added/launch/openshift-node-name.sh $JBOSS_HOME/bin/launch
-cp $BATS_TEST_DIRNAME/../../../../os-logging/added/launch/logging.sh $JBOSS_HOME/bin/launch
+cp $BATS_TEST_DIRNAME/../../../../test-common/logging.sh $JBOSS_HOME/bin/launch
 cp $BATS_TEST_DIRNAME/../../../../os-eap-launch/added/launch/datasource-common.sh $JBOSS_HOME/bin/launch
 cp $BATS_TEST_DIRNAME/../../../added/launch/launch-common.sh $JBOSS_HOME/bin/launch
 cp $BATS_TEST_DIRNAME/../../../added/launch/tx-datasource.sh $JBOSS_HOME/bin/launch
