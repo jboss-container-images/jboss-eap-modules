0d7c0d0a63175e0c76523867718d745c6220bc7f
Kabir Khan <kkhan@redhat.com>
2019-05-13 19:12:43 +0100

    Add the SSL server identity and https-listener via CLI

--- os-eap7-launch/added/launch/https.sh
+++ os-eap7-launch/added/launch/https.sh
@@ -19,16 +19,35 @@ function configureEnv() {
 }
 
 function configure_https() {
-  local ssl="<!-- No SSL configuration discovered -->"
-  local https_connector="<!-- No HTTPS configuration discovered -->"
-
   if [ "${CONFIGURE_ELYTRON_SSL}" == "true" ]; then
     echo "Using Elytron for SSL configuration."
     return 
   fi
 
+  local valid="false"
   if [ -n "${HTTPS_PASSWORD}" -a -n "${HTTPS_KEYSTORE_DIR}" -a -n "${HTTPS_KEYSTORE}" ]; then
+    valid="true"
+  elif [ -n "${HTTPS_PASSWORD}" -o -n "${HTTPS_KEYSTORE_DIR}" -o -n "${HTTPS_KEYSTORE}" ]; then
+    log_warning "Partial HTTPS configuration, the https connector WILL NOT be configured."
+  fi
 
+  configureSsl "${valid}"
+  configureHttps "${valid}"
+}
+
+function configureSsl() {
+  local valid="${1}"
+  if [ $(isConfigurationViaMarkerReplacement "<!-- ##SSL## -->") -eq 1 ]; then
+    configureSslXml "${valid}"
+  else
+    configureSslCli "${valid}"
+  fi
+}
+
+function configureSslXml() {
+  local ssl="<!-- No SSL configuration discovered -->"
+  local valid="${1}"
+  if [ ${valid} = "true" ]; then
     if [ -n "$HTTPS_KEYSTORE_TYPE" ]; then
       keystore_provider="provider=\"${HTTPS_KEYSTORE_TYPE}\""
     fi
@@ -37,12 +56,71 @@ function configure_https() {
                         <keystore ${keystore_provider} path=\"${HTTPS_KEYSTORE_DIR}/${HTTPS_KEYSTORE}\" keystore-password=\"${HTTPS_PASSWORD}\"/>\n\
                     </ssl>\n\
                 </server-identities>"
-
-    https_connector="<https-listener name=\"https\" socket-binding=\"https\" security-realm=\"ApplicationRealm\" proxy-address-forwarding=\"true\"/>"
-  elif [ -n "${HTTPS_PASSWORD}" -o -n "${HTTPS_KEYSTORE_DIR}" -o -n "${HTTPS_KEYSTORE}" ]; then
-    log_warning "Partial HTTPS configuration, the https connector WILL NOT be configured."
   fi
-
   sed -i "s|<!-- ##SSL## -->|${ssl}|" $CONFIG_FILE
-  sed -i "s|<!-- ##HTTPS_CONNECTOR## -->|${https_connector}|" $CONFIG_FILE
+}
+
+function configureSslCli() {
+  if [ ${valid} != "true" ]; then
+    return
+  fi
+  local ssl_resource="/core-service=management/security-realm=ApplicationRealm/server-identity=ssl"
+  
+  local ssl_add="$ssl_resource:add(keystore-path=\"${HTTPS_KEYSTORE_DIR}/${HTTPS_KEYSTORE}\", keystore-password=\"${HTTPS_PASSWORD}\""
+  if [ -n "$HTTPS_KEYSTORE_TYPE" ]; then
+    ssl_add="${ssl_add}, keystore-provider=\"${HTTPS_KEYSTORE_TYPE}\""
+  fi
+  ssl_add="${ssl_add})"
+
+  local ssl="
+  if (outcome == success) of $ssl_resource:read-resource
+    batch
+    ${ssl_resource}:remove
+    ${ssl_add}
+    run-batch
+  else
+    ${ssl_add}
+  end-if
+  "
+  cat << EOF >> ${CLI_SCRIPT_FILE}
+	  ${ssl}
+EOF
+}
+
+function configureHttps() {
+  local valid="${1}"
+  if [ $(isConfigurationViaMarkerReplacement "<!-- ##HTTPS_CONNECTOR## -->") -eq 1 ]; then
+    configureHttpsXml "${valid}"
+  else
+    configureHttpsCli "${valid}"
+  fi
+}
+
+function configureHttpsXml() {
+  local https_connector="<!-- No HTTPS configuration discovered -->"
+  local valid="${1}"
+  if [ ${valid} = "true" ]; then
+    https_connector="<https-listener name=\"https\" socket-binding=\"https\" security-realm=\"ApplicationRealm\" proxy-address-forwarding=\"true\"/>"
+  fi
+  sed -i "s|<!-- ##HTTPS_CONNECTOR## -->|${https_connector}|" $CONFIG_FILE     
+}
+
+function configureHttpsCli() {
+  local valid="${1}"
+  if [ ${valid} != "true" ]; then
+    return
+  fi
+  # Add the https listener to all undertow servers
+  local https_connector="
+   for serverName in /subsystem=undertow:read-children-names(child-type=server)
+    if (result == []) of /subsystem=undertow/server=\$serverName:read-children-names(child-type=https-listener)
+      /subsystem=undertow/server=\$serverName/https-listener=https:add(security-realm=ApplicationRealm, socket-binding=https, proxy-address-forwarding=true)
+    else
+      echo \"There is already an undertow https-listener for the '\$serverName' server so we are not adding it\" 
+    end-if
+  done
+"
+  cat << EOF >> ${CLI_SCRIPT_FILE}
+	  ${https_connector}
+EOF
 }
