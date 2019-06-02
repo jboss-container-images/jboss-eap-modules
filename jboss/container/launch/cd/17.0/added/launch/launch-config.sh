0fc66fbbb5b509ef87b94b966632111bc143a367
Kabir Khan <kkhan@redhat.com>
2019-06-02 10:28:12 +0100

    Some comments

--- jboss/container/launch/cd/17.0/added/launch/launch-config.sh
+++ jboss/container/launch/cd/17.0/added/launch/launch-config.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Centralised configuration file to set variables that affect the launch scripts in wildfly-cekit-modules.
+
+# Scripts that modify the configuration. Either via xml marker replacement or via CLI commands.
+# wildfly-cekit-modules will look for each of the listed files and run them if they exist.
 CONFIG_SCRIPT_CANDIDATES=(
   $JBOSS_HOME/bin/launch/backward-compatibility.sh
   $JBOSS_HOME/bin/launch/configure_extensions.sh
@@ -21,9 +25,14 @@ CONFIG_SCRIPT_CANDIDATES=(
   $JBOSS_HOME/bin/launch/access_log_valve.sh
   $JBOSS_HOME/bin/launch/filters.sh
 )
+# Scripts that modify the runtime environment variables.
+# wildfly-cekit-modules will look for each of the listed files and run them if they exist.
 ENV_SCRIPT_CANDIDATES=(
   /opt/run-java/proxy-options
   $JBOSS_HOME/bin/launch/jboss_modules_system_pkgs.sh
 )
+# The server configuration file to use. If not set, wildfly-cekit-modules defaults to standalone.xml.
+# For EAP we want standalone-openshift.xml
 WILDFLY_SERVER_CONFIGURATION=standalone-openshift.xml
+# The configuration adjustment mode. For EAP we want both xml marker replacement and CLI commands.
 CONFIG_ADJUSTMENT_MODE="xml_cli"
