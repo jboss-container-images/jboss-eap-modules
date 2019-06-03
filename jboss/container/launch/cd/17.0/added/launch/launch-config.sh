987782da54230008363987a85bb62c02f64283e6
Yeray Borges <yborgess@redhat.com>
2019-06-03 11:51:54 +0100

    Added module that enables CLI configuration

---
+++ jboss/container/launch/cd/17.0/added/launch/launch-config.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+# Centralised configuration file to set variables that affect the launch scripts in wildfly-cekit-modules.
+
+# Scripts that modify the configuration. Either via xml marker replacement or via CLI commands.
+# wildfly-cekit-modules will look for each of the listed files and run them if they exist.
+CONFIG_SCRIPT_CANDIDATES=(
+  $JBOSS_HOME/bin/launch/backward-compatibility.sh
+  $JBOSS_HOME/bin/launch/configure_extensions.sh
+  $JBOSS_HOME/bin/launch/passwd.sh
+  $JBOSS_HOME/bin/launch/messaging.sh
+  $JBOSS_HOME/bin/launch/datasource.sh
+  $JBOSS_HOME/bin/launch/resource-adapter.sh
+  $JBOSS_HOME/bin/launch/admin.sh
+  $JBOSS_HOME/bin/launch/ha.sh
+  $JBOSS_HOME/bin/launch/jgroups.sh
+  $JBOSS_HOME/bin/launch/https.sh
+  $JBOSS_HOME/bin/launch/elytron.sh
+  $JBOSS_HOME/bin/launch/json_logging.sh
+  $JBOSS_HOME/bin/launch/configure_logger_category.sh
+  $JBOSS_HOME/bin/launch/security-domains.sh
+  $JBOSS_HOME/bin/launch/keycloak.sh
+  $JBOSS_HOME/bin/launch/deploymentScanner.sh
+  $JBOSS_HOME/bin/launch/ports.sh
+  $JBOSS_HOME/bin/launch/access_log_valve.sh
+  $JBOSS_HOME/bin/launch/filters.sh
+)
+# Scripts that modify the runtime environment variables.
+# wildfly-cekit-modules will look for each of the listed files and run them if they exist.
+ENV_SCRIPT_CANDIDATES=(
+  /opt/run-java/proxy-options
+  $JBOSS_HOME/bin/launch/jboss_modules_system_pkgs.sh
+)
+# The server configuration file to use. If not set, wildfly-cekit-modules defaults to standalone.xml.
+# For EAP we want standalone-openshift.xml
+WILDFLY_SERVER_CONFIGURATION=standalone-openshift.xml
+# The configuration adjustment mode. For EAP we want both xml marker replacement and CLI commands.
+CONFIG_ADJUSTMENT_MODE="xml_cli"
