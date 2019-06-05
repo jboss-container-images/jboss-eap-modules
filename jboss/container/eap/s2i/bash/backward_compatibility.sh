13e501d24021f16435fb25b62cc11805c02c9e2c
Yeray Borges <yborgess@redhat.com>
2019-06-05 09:23:34 +0100

    Changes to be able to use S2I common module and enable Drivers via CLI support

--- jboss/container/eap/s2i/bash/backward_compatibility.sh
+++ jboss/container/eap/s2i/bash/backward_compatibility.sh
@@ -2,6 +2,8 @@
 # Set up Hawkular for java s2i builder image
 set -e
 
-ln -s /opt/jboss/container/eap/s2i/install-common.sh /usr/local/s2i/install-common.sh
+ln -s /opt/jboss/container/common/s2i/install-common.sh /opt/jboss/container/eap/s2i/install-common.sh
+ln -s /opt/jboss/container/common/s2i/install-common.sh /usr/local/s2i/install-common.sh
 
 chown -h jboss:root /usr/local/s2i/install-common.sh
+chown -h jboss:root /opt/jboss/container/eap/s2i/install-common.sh
