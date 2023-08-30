4275c9af22197226974fd6cb85eb24769057fe99
Ruben Novelli <rnovelli@redhat.com>
2023-08-30 10:35:17 +0200

    [CLOUD-4198] Add new 2022 certificate

--- jboss/container/eap/jdk-cert-remove/configure.sh
+++ jboss/container/eap/jdk-cert-remove/configure.sh
@@ -4,4 +4,5 @@ set -e
 
 # Download the root certificate and update the certificates.
 rm -rf /etc/pki/ca-trust/source/anchors/RH-IT-Root-CA.crt
+rm -rf /etc/pki/ca-trust/source/anchors/2022-IT-Root-CA.pem
 update-ca-trust
