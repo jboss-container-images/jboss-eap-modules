298144e4c1621088121b6db6f673b3dd329efa53
Ruben Novelli <rnovelli@redhat.com>
2024-02-29 08:46:28 +0100

    [EAPPROD-895] Update certificate

--- jboss/container/eap/jdk-cert-remove/configure.sh
+++ jboss/container/eap/jdk-cert-remove/configure.sh
@@ -3,6 +3,6 @@
 set -e
 
 # Download the root certificate and update the certificates.
-rm -rf /etc/pki/ca-trust/source/anchors/RH-IT-Root-CA.crt
+rm -rf /etc/pki/ca-trust/source/anchors/Current-IT-Root-CAs.pem
 rm -rf /etc/pki/ca-trust/source/anchors/2022-IT-Root-CA.pem
 update-ca-trust
